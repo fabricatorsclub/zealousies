127f500915a50cb071c7c6ec504d3cc7b38d4641
Gopi Raja <gopi@inkmonk.com>
2018-09-23 13:58:47 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './assets/stylesheets/screen.css';
 import logo from './assets/images/logo.svg';
 import mountain from './assets/images/mountain.svg';
 import boy from './assets/images/boy.svg';
+import mouse from './assets/images/mouse.svg';
 
 class Wrapper extends Component {
   constructor(props) {
@@ -35,7 +36,7 @@ class Wrapper extends Component {
           width: width,
           height: height,
           position: 'relative',
-          overflow: 'hidden',
+          // overflow: 'hidden',
         }}
       >
         <div style={{ position: 'fixed', top: 0, right: 0 }}>
@@ -73,30 +74,11 @@ class App extends Component {
                 <img src={mountain} alt="mountain" className="mountain" />
                 <img src={boy} alt="boy" className="boy" />
               </div>
-            </div>
-          </div>
-        </Wrapper>
-        <Wrapper>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="branding" alt="Zealousies" />
-              <a>Get Access</a>
-            </header>
-            <div className="home-content">
-              <h1>
-                Even Harry Potter <br />
-                needs a Dumbledore to <br />
-                mentor him to find himself.
-              </h1>
-              <p className="greyText">
-                We are building a universe of Alfreds, <br />
-                Dumbledores, Dronacharyas who can <br />
-                help you become a superhero!
-              </p>
 
-              <div className="heroImage">
-                <img src={mountain} alt="mountain" className="mountain" />
-                <img src={boy} alt="boy" className="boy" />
+              <div className="scroll-info greyText">
+                Scroll to
+                <img src={mouse} alt="mouse" />
+                know more
               </div>
             </div>
           </div>
